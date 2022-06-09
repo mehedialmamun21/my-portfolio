009b72a5c22340d071dcdc594eb446e97c14ffeb
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-06-09 19:21:20 +0600

    projects section added with cards

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
 import fileDownload from 'js-file-download';
 import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
+import bannerImg from '../../assets/images/bannerImg.jpg';
+import downloadIcon from '../../assets/images/download_icon.png';
+import Projects from '../Projects/Projects';
 
 const Home = () => {
+
+    const [state] = useState({
+        titleOne: "Hi there,",
+        titleTwo: "I'M",
+        titleThree: "Md. Mehedi Al Mamun",
+    })
 
     const download = (e) => {
         e.preventDefault();
@@ -13,28 +23,55 @@ const Home = () => {
             responseType: "blob"
         })
             .then((res) => {
-                // console.log(res);
                 fileDownload(res.data, "Mehedi_Web_Developer_Resume.pdf")
             })
     }
 
     return (
-        <div className='lg:flex bg-black py-10'>
-            <div className='w-50 lg:ml-16'>
-                <img src="https://i.postimg.cc/RFbTPS9W/banner-Img.jpg" alt="" width="700" className='rounded' />
-            </div>
-            <div className='w-50 ml-20 mt-20'>
-                <h1 className='text-xl text-white text-center'>Hi there !</h1>
-                <h1 className='text-3xl font-semibold text-white text-center'>I'M <span className='text-primary'> Mehedi Al Mamun</span> </h1> <br />
-                <div className='text-center mt-10'>
-                    <Link to="/contact"><button type='button' className='btn btn-accent hover:text-white font-bold hover:bg-secondary text-black'>Hire Me</button></Link>
-                </div> <br />
-                <div className='text-center'>
-                    <button onClick={(e) => download(e)} type='button' className='btn btn-secondary hover:bg-accent hover:text-black text-white font-semibold'>
-                        Download Resume
-                        <img src="https://i.postimg.cc/wTSgCLFV/download-icon.png" alt="" width="30" className='ml-2' />
-                    </button>
+        <div>
+            <div className='lg:flex bg-black py-10'>
+
+                <div className='w-50 lg:ml-16'>
+                    <img src={bannerImg} alt="" width="700" className='rounded' />
                 </div>
+
+                <div className='w-50 lg:ml-40 mt-20'>
+                    <h2>
+                        <div className='text-xl text-white text-center'>{state.titleOne}</div>
+                        <div className='text-2xl font-semibold text-white text-center'><span>{state.titleTwo}</span> </div>
+                        <div className='text-3xl font-semibold text-center text-orange-500'>{state.titleThree}</div>
+                    </h2>
+
+                    <div className='text font-bold text-2xl text-blue-500 text-center mt-5'>
+                        <Typewriter
+                            options={{
+                                autoStart: true,
+                                loop: true,
+                                delay: 40,
+                                strings: [
+                                    "Front-End Web Developer",
+                                    "Tech Enthusiast"
+                                ],
+                            }} />
+                    </div>
+
+                    <div className='text-center mt-10'>
+                        <Link to="/contact"><button type='button' className='btn btn-secondary hover:text-black font-bold hover:bg-accent text-white'>Hire Me</button></Link>
+                    </div>
+
+                    <br />
+
+                    <div className='text-center'>
+                        <button onClick={(e) => download(e)} type='button' className='btn btn-accent hover:bg-secondary hover:text-white text-black font-semibold'>
+                            Download Resume
+                            <img src={downloadIcon} alt="" width="30" className='ml-2' />
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+            <div>
+                <Projects />
             </div>
         </div>
     );
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import manufacturer from '../../assets/images/manufacturer.png';
+import warehouse from '../../assets/images/warehouse.png';
+import cms from '../../assets/images/cms.png';
 
 const Projects = () => {
     return (
         <div>
-            <h1 className='text-center'>From Projects page</h1>
+            <h1 className='text-center text-3xl my-10'>Recent <span className='text-orange-500'>Projects</span> </h1>
+            <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-6 mx-20'>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10 mb-5">
+                        <img src={manufacturer} alt="manufacturer" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center bg-gray-500 text-white">
+                        <h2 class="card-title text-2xl">Manufacturer Website</h2> <br />
+                        <p>This website is build for a manufacturer company who manufactures a particular products and sell it on their website</p>
+                    </div>
+                </div>
+
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10 mb-5">
+                        <img src={warehouse} alt="warehouse" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center bg-gray-500 text-white">
+                        <h2 class="card-title text-2xl">Warehouse Management</h2> <br />
+                        <p>This site is build for selling modile phone. User can buying phone from here and admin can upload or delete products.</p>
+                    </div>
+                </div>
+
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure class="px-10 pt-10">
+                        <img src={cms} alt="cms" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center bg-gray-500 text-white">
+                        <h2 class="card-title text-2xl mb-5">Service based Content Management System</h2>
+                        <p>This site is build for offering Dental services for users. User can booking consaltant and make payment by cards.</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
