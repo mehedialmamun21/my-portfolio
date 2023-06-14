1e954372f92526c883a9fb43e49f31dca84b8f60
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-06-15 04:52:12 +0600

    project section updated with cms to dental service provider name

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -5,7 +5,7 @@ const Projects = () => {
     const projects = [
         { id: 1, img: "https://i.postimg.cc/PJtGWxYL/manufacturer.png", name: 'Manufacturer Website', about: "This website is build for a manufacturer company who manufactures some products and sell those here." },
         { id: 2, img: "https://i.postimg.cc/7Yr6GM5h/warehouse.png", name: 'Warehouse Management', about: "This site is build for selling modile phone. Customers can buy phone from here and admin can upload or delete products." },
-        { id: 3, img: "https://i.postimg.cc/c4tXWFx9/cms.png", name: 'CMS System', about: "This site is build for offering Dental services for users. User can booking consaltant and make payment by cards." },
+        { id: 3, img: "https://i.postimg.cc/c4tXWFx9/cms.png", name: 'Dental Service Provider', about: "This site is build for offering Dental services for users. User can booking consaltant and make payment by cards." },
         { id: 4, img: "https://i.postimg.cc/c4tXWFx9/cms.png", name: 'Leaf Disease Classification', about: "This site is build for predicting crop disease. Here, we've worked on Potato Leaf Disease prediction with CNN algorithm." },
         { id: 5, img: "https://i.postimg.cc/c4tXWFx9/cms.png", name: 'License Plate Detection', about: "By this, we can detect vehicles (car's) number plate accurately." }
     ]
